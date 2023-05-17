af0cec0759b27c6cbc6d631d74cabff0abd09d30
camvenli <wenjian.li@dreampix.cn>
2023-05-17 15:27:32 +0800

    first：fileos

---
+++ fileos.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+#include<fstream>
+
+
+
+
+int main() {
+	std::ofstream  out;
+	out.open("1.txt");
+	string str;
+	cin >> str;
+	out << str << endl;
+	ifstream in;
+	in.open("1.txt");
+	string str1;
+	in >> str1;
+
+	cout << "str1=" << str1 << endl;
+	system("pause");
+
+	return 0;
+}
